b93a6e0a6bea25f9c2a5f6c5253e7e5a76da1d2e
Ryuji Oda <ryuji.oda@gmail.com>
2021-02-10 09:47:43 +0900

    constrain number and dot input

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,15 +3,35 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var isDecimal = false
+    var isNumeric = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun onDigit(view: View) {
-        tvInput
+        tvInput.append((view as Button).text)
+        isNumeric = true
+    }
+
+    fun onClear (view: View){
+        tvInput.text = ""
+        isDecimal = false
+        isNumeric = false
+    }
+
+    fun onDecimalPoint (view: View) {
+        if (isNumeric && !isDecimal) {
+            tvInput.append((view as Button).text)
+            isDecimal = true
+        }
     }
 }
